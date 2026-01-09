7ecc7492446ab81a039167613cceece59c96c720
Daniel Frankcom <frankcom@amazon.com>
2026-01-09 14:23:20 -0800

    Call out change in update_or_create method

--- aurora_dsql_tortoise/common/models.py
+++ aurora_dsql_tortoise/common/models.py
@@ -18,12 +18,16 @@ class DSQLModel(Model):
         using_db=None,
         **kwargs: Any,
     ) -> tuple[Self, bool]:
-        """DSQL-compatible update_or_create using OCC instead of SELECT FOR UPDATE."""
+        """DSQL-compatible update_or_create using OCC instead of SELECT FOR UPDATE.
+
+        Based on https://github.com/tortoise/tortoise-orm/blob/1d2400bb3daff7b1aa34884062ca0e936ea214b2/tortoise/models.py#L1165
+        """
         if not defaults:
             defaults = {}
         db = using_db or cls._choose_db(True)
         async with in_transaction(connection_name=db.connection_name) as connection:
-            instance = await cls.filter(**kwargs).using_db(connection).get_or_none()
+            # Change is here, replaced select_for_update() with filter call to get QuerySet.
+            instance = await cls.filter(**kwargs).using_db(connection).get_or_none(**kwargs)
             if instance:
                 await instance.update_from_dict(defaults).save(using_db=connection)
                 return instance, False
