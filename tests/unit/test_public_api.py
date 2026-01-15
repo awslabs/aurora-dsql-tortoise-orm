f146b79c553114bc33ae27d8eedd4934217a80fd
Daniel Frankcom <daniel@frankcom.ca>
2026-01-15 13:43:23 -0800

    Define public API (#5)
    Most of this package is intended to be used by Tortoise ORM, and not
    directly by the user.
    
    I've added an `__all__` definition at the top level which limits what is
    made available for default import statements.
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

---
+++ tests/unit/test_public_api.py
@@ -0,0 +1,13 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+
+def test_public_api():
+    import aurora_dsql_tortoise
+
+    assert set(aurora_dsql_tortoise.__all__) == {
+        "DSQLModel",
+        "register_backends",
+        "register_asyncpg",
+        "register_psycopg",
+    }
